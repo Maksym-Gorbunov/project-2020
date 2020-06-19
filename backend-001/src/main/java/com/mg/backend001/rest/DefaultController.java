88a89c090b16a7be44f27d40ddd5f682882c63fb
Maksym-Gorbunov <maxsverige@hotmail.com>
2020-06-20 00:14:52 +0200

    backend-001 mysql connected

---
+++ backend-001/src/main/java/com/mg/backend001/rest/DefaultController.java
@@ -0,0 +1,19 @@
+package com.mg.backend001.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+    @Autowired
+    private Environment env;
+
+    @GetMapping
+    public String healthChecker(){
+        return env.getProperty("spring.application.name")+" project is running";
+    }
+}
