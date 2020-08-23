327588ff0c60bd8988d79e7bf309e95bb0cbf1b1
Maksym-Gorbunov <maxsverige@hotmail.com>
2020-08-23 21:42:20 +0200

    Vechicle added

---
+++ backend-001/src/main/java/com/mg/backend001/entity/Vehicle.java
@@ -0,0 +1,24 @@
+package com.mg.backend001.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@ToString
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String registerNumber;
+
+    private String model;
+    private String color;
+}
